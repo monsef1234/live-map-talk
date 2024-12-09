63af156139b4e6d7f4e071033d1f33fbf32cfba9
monsef1234 <monsef4566@gmail.com>
2024-12-09 04:51:19 +0100

    fix: use PORT from env, chore: add env and env exemple files

--- backend/index.ts
+++ backend/index.ts
@@ -56,7 +56,7 @@ io.on("connection", (socket: Socket) => {
 
 });
 
-httpServer.listen(3003, () => {
+httpServer.listen(process.env.PORT, () => {
     console.log("Server is running on port 3003");
 });
 
