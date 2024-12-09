9ad657169e59576ddf177b683aa997e4142a1460
monsef1234 <monsef4566@gmail.com>
2024-12-09 06:19:05 +0100

    fix: refactoring code

--- frontend/src/views/home/Index.vue
+++ frontend/src/views/home/Index.vue
@@ -38,7 +38,7 @@
 import { defineComponent } from "vue";
 import { socketEvents } from "../../services/socket";
 import { useStore } from "../../store/store";
-import { User } from "../../types/user";
+// import { User } from "../../types/user";
 
 export default defineComponent({
   name: "Home",
